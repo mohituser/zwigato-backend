67c910ad491a4245a82e4ebae5375d18f34823b1
mohituser <122739741+mohituser@users.noreply.github.com>
2024-02-12 17:01:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,8 +12,8 @@ connectWithDb();
 
 app.use(cors({
     // origin:"https://zwigato-frontend-five.vercel.app",
-    // origin:"*",
-       origin:"http://localhost:3000/",
+    origin:"*",
+       // origin:"http://localhost:3000/",
     credentials:true,     
     }));
 app.use(express.json());
